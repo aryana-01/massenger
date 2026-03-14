dfe123b3a5f60e36156f208a814d8a9c158c2668
aryana-01 <raryann11@gmail.com>
2026-03-14 19:02:18 +0530

    Refactor chat.js for socket.io path and function simplification
    Refactor chat.js to use custom socket.io path and simplify functions.

--- chat.js
+++ chat.js
@@ -1,28 +1,7 @@
-const socket = io();
+const socket = io({ path: "/api/socketio" });
 
-function login(){
-  let username = document.getElementById("username").value;
-  if (!username) return alert("Enter email");
-  socket.emit("join", username);
-  document.getElementById("loginDiv").style.display = "none";
-  document.getElementById("chatDiv").style.display = "flex";
-}
+function login() { ... }
+function sendMessage() { ... }
 
-function sendMessage(){
-  let msg = document.getElementById("msg").value;
-  if (!msg) return;
-  socket.emit("chat message", msg);
-  document.getElementById("msg").value = "";
-}
-
-socket.on("chat message", (msg) => {
-  let div = document.createElement("div");
-  div.className = "message";
-  div.innerText = msg;
-  document.getElementById("messages").appendChild(div);
-});
-
-socket.on("blocked", (msg) => {
-  alert(msg);
-  location.reload();
-});
+socket.on("chat message", msg => { ... });
+socket.on("blocked", msg => { ... });
